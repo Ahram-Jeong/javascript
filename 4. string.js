81241966f18992a28dbfc1b048d1293ffeb7779d
Ahram-Jeong <ahram1215@gmail.com>
2023-12-25 22:47:18 +0900

    20231225 - 문자열 타입 (string)

---
+++ 4. string.js
@@ -0,0 +1,23 @@
+// 문자열 타입
+let string = '조승연';
+console.log(string);
+string = `Woodz`;
+console.log(string);
+
+// 특수 문자 출력
+string = "'우즈락하자!'";
+console.log(string);
+
+string = 'Hello, \n승연아!\t\t군면제 가자!\\';
+console.log(string);
+string = '\u09AB';
+console.log(string);
+
+// 템플릿 리터럴 (Template Literal) ``
+let id = '우즈';
+let greetings = '"안녕! ' + id + '\n즐거운 하루 보내요:)"';
+console.log(greetings);
+
+greetings = `"안녕! ${id},
+즐거운 하루 보내요:)"`
+console.log(greetings);
