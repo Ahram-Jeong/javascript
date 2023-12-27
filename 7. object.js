6dc0af9144a5c517603111130686eab17e0c0840
Ahram-Jeong <ahram1215@gmail.com>
2023-12-27 23:35:04 +0900

    20231227 - 객체 타입 (object)

---
+++ 7. object.js
@@ -0,0 +1,20 @@
+let name = 'woodz';
+let color = 'orange, blue';
+let display = '🧡💙';
+
+let woodz = {
+    name : 'woodz',
+    color : 'orange, blue',
+    display : '🧡💙'
+};
+console.log(woodz);
+console.log(woodz.name);
+console.log(woodz.color);
+console.log(woodz.display);
+
+let orange = {
+    name : '오렌지',
+    color : 'orange',
+    display : '🍊'
+};
+console.log(orange);
