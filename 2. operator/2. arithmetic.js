7cdf868a374d88be6b5128402a47de0e7e6c321d
Ahram-Jeong <ahram1215@gmail.com>
2024-01-21 10:03:02 +0900

    20240121 - 산술 연산자 (arithmetic)

---
+++ 2. operator/2. arithmetic.js
@@ -0,0 +1,21 @@
+// 산술 연산자 (Arithmetic operators)
+// + 더하기
+// - 빼기
+// * 곱하기
+// / 나누기
+// % 나머지 값
+// ** 지수
+
+console.log(5 + 2);
+console.log(5 - 2);
+console.log(5 * 2);
+console.log(5 / 2);
+console.log(5 % 2);
+console.log(5 ** 2);
+console.log(Math.pow(5, 2)); // ** 지수
+
+// + 연산자 주의점!
+let text1 = '조' + '승연';
+console.log(text1);
+let text2 = '1' + 1; // 숫자 + 문자열 -> 문자열로 변환
+console.log(text2); // 11 -> string 타입
