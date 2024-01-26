5cb00b04997e7eec33d1bcd78c0886dc2b4968ad
Ahram-Jeong <ahram1215@gmail.com>
2024-01-26 22:47:34 +0900

    20240126 - 제어문 퀴즈 (odd, even 출력)

---
+++ 3. control/3. quiz.js
@@ -0,0 +1,16 @@
+// 퀴즈!
+let num = 2;
+
+// num이 짝수이면 👍, 홀수이면 👎 출력
+
+// 1. if
+if (num%2 === 0) {
+    console.log('👍');
+} else {
+    console.log('👎');
+}
+
+// 2. ternary
+// num%2 === 0 ? console.log('👍') : console.log('👎');
+let emoji = num%2 === 0 ? '👍' : '👎';
+console.log(emoji);
