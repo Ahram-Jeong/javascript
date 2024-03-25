f978bd632966735ed38007b830507628e6aaceba
Ahram-Jeong <ahram1215@gmail.com>
2024-03-25 11:30:00 +0900

    20240325 - boolean 함수

---
+++ 7. built-in/3. boolean.js
@@ -0,0 +1,9 @@
+// Boolean
+const isTrue = new Boolean(true);
+// const isTrue = true;
+console.log(isTrue.valueOf());
+
+/**
+ * Falshy : 0, -0, null, NaN, undefined, ''
+ * Truthy : 1, -1, '0', 'false', [], {}
+ */
