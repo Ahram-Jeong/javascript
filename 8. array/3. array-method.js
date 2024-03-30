7e70c45d57fb55943382f147ec38422148c56e3e
Ahram-Jeong <ahram1215@gmail.com>
2024-03-30 16:47:22 +0900

    20240330 - Array 함수 2

--- 8. array/3. array-method.js
+++ 8. array/3. array-method.js
@@ -3,6 +3,9 @@
  * point : 배열 자체를 변경 or 새로운 배열 반환
  */
 
+/**
+ * 배열 자체를 변경
+ */
 const fruits = ['🍋', '🍑', '🍍'];
 
 // 특정 오브젝트가 배열인지 체크
@@ -42,3 +45,52 @@ console.log(deleted); // [ '🍑' ]
 
 fruits.splice(1, 0, '🍓', '🍏'); // 삭제하지는 않고 추가만
 console.log(fruits); // 배열 자체를 수정 (update), [ '🍋', '🍓', '🍏', '🍍' ]
+
+/**
+ * 새로운 배열을 반환
+ */
+let newArr = fruits.slice(0, 2);
+console.log(fruits); // [ '🍋', '🍓', '🍏', '🍍' ]
+console.log(newArr); // [ '🍋', '🍓' ]
+newArr = fruits.slice();
+console.log(newArr); // 배열 전체를 새롭게 반환
+
+// 여러 개의 배열을 이어 붙임
+const arr1 = [1, 2, 3];
+const arr2 = [4, 5, 6];
+const arr3 = arr1.concat(arr2);
+console.log(arr1);
+console.log(arr2);
+console.log(arr3);
+
+// 배열의 순서를 거꾸로
+const arr4 = arr3.reverse();
+console.log(arr4);
+
+console.clear();
+// 중첩 배열을 하나의 flat한 배열로 변환
+let arr = [
+    [1, 2, 3],
+    [4, [5, 6]]
+];
+console.log(arr); // [ [ 1, 2, 3 ], [ 4, [ 5, 6 ] ] ]
+console.log(arr.flat()); // 한 단계까지만 flat => [ 1, 2, 3, 4, [ 5, 6 ] ]
+console.log(arr.flat(2)); // 두 단계 flat => [ 1, 2, 3, 4, 5, 6 ]
+arr = arr.flat(2)
+
+/**
+ * 특정 값으로 배열 채우기
+ * fill() : 배열 자체를 수정
+ */
+arr.fill(0);
+console.log(arr); // [ 0, 0, 0, 0, 0, 0 ]
+arr.fill('🍰', 1, 3); // 1 <= i < 3
+console.log(arr); // [ 0, '🍰', '🍰', 0, 0, 0 ]
+arr.fill('🍕', 1); // 1 <= i
+console.log(arr); // [ 0, '🍕', '🍕', '🍕', '🍕', '🍕' ]
+
+// 배열을 문자열로 합치기
+let text = arr.join();
+console.log(text); // 0,🍕,🍕,🍕,🍕,🍕
+text = arr.join(' | ');
+console.log(text); // 0 | 🍕 | 🍕 | 🍕 | 🍕 | 🍕
